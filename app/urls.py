555984ed10714811904df052eb74346e10bdd4de
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2020-05-28 12:24:39 -0300

    01 Aplicação Inicial

---
+++ app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views.tarefa_views import *
+
+urlpatterns = [
+    path('listar_tarefas/', listar_tarefas, name='listar_tarefas'),
+    path('cadastrar_tarefa/', cadastrar_tarefa, name='cadastrar_tarefa'),
+    path('editar_tarefa/<int:id>', editar_tarefa, name='editar_tarefa'),
+    path('remover_tarefa/<int:id>', remover_tarefa, name='remover_tarefa'),
+]
